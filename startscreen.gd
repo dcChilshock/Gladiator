ff7e07b5a4553dc228292d4267adafd91df7aaf3
dcChilshock <densonchilders123@gmail.com>
2023-12-18 19:43:37 -0600

    HUD design, characters, collision
    get start button to work if I ever read this idk

---
+++ startscreen.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+
+func _on_start_pressed():
+	pass
+	#make character enter first or stored scene data
+
+
+func _on_exit_pressed():
+	get_tree().quit()
