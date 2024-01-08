bbee694cfe2b787cd5101d402b5be9152cdd50d2
dcChilshock <densonchilders123@gmail.com>
2024-01-08 12:52:40 -0600

    dfhgdgfhf

--- test_enemy.gd
+++ test_enemy.gd
@@ -6,15 +6,15 @@ const speed = 50
 
 @onready var nav_agent := $NavigationAgent2D as NavigationAgent2D
 
-func _ready()->void:
+func _ready():
 	makepath()
 
-func _physics_process(_delta: float) -> void: 
+func _physics_process(_delta: float): 
 	var dir = to_local(nav_agent.get_next_path_position()).normalized()
 	velocity = dir * speed
 	move_and_slide()
 
-func makepath() -> void:
+func makepath():
 	nav_agent.target_position = player.global_position
 
 func _on_timer_timeout():
